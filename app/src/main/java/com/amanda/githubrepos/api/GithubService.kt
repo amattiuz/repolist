378af1979037ac4baccc66ba7f9ab0dd2a6312a0
Amanda Mattiuz <amanca@amankali.kali.amanca>
2021-01-06 08:01:22 -0500

    Initial commit. Setting up gradle dependencies and creating packages.

---
+++ app/src/main/java/com/amanda/githubrepos/api/GithubService.kt
@@ -0,0 +1,35 @@
+package com.amanda.githubrepos.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+//Github REST API communication setup via Retrofit.
+interface GithubService {
+
+    @GET("search/repositories?sort=stars")
+    suspend fun listRepos(): Object
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
+}
